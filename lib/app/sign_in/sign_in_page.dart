597c480f7bdfc462f6359294141d768f05f39a3d
Eventyret <simen.dehlin@gmail.com>
2020-05-10 22:37:09 +0100

    Worked with some containers

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -4,7 +4,26 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Center(child: Text('Time Tracker'))),
+      appBar: AppBar(
+        title: Center(
+          child: Text('Time Tracker'),
+        ),
+        elevation: 2.0,
+      ),
+      body: Container(
+        color: Colors.yellow,
+        child: Column(
+          children: <Widget>[
+            Container(
+              color: Colors.orange,
+              child: SizedBox(
+                width: 100.0,
+                height: 100.0,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }

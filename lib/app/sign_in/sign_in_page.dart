8a2f6647ad1f3a99a169d4300f9e620a5c907d02
Eventyret <simen.dehlin@gmail.com>
2020-05-10 22:24:49 +0100

    Hooked up page

---
+++ lib/app/sign_in/sign_in_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Center(child: Text('Time Tracker'))),
+    );
+  }
+}

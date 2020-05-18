534764b9fdfe7ebead89145b6b3651d2f4e9e593
Eventyret <simen.dehlin@gmail.com>
2020-05-18 15:32:10 +0100

    Extracted custom signin button

---
+++ lib/common/custom_raised_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomRaisedButton extends StatelessWidget {
+  final Widget child;
+  final Color color;
+  final double borderRadius;
+  final VoidCallback onPressed;
+  const CustomRaisedButton({
+    this.onPressed,
+    this.child,
+    this.color,
+    this.borderRadius,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      child: child,
+      color: color,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(borderRadius),
+        ),
+      ),
+      onPressed: onPressed,
+    );
+  }
+}

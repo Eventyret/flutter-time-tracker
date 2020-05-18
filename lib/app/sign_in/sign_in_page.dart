0ea1121571b5dcafb60505484117f5bc24ae1c6f
Eventyret <simen.dehlin@gmail.com>
2020-05-18 16:01:17 +0100

    Added or and Anonomoys button

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -56,6 +56,26 @@ class SignInPage extends StatelessWidget {
             color: Colors.teal[700],
             onPressed: () {},
           ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Text(
+            'or',
+            style: TextStyle(
+              fontSize: 14.0,
+              color: Colors.black87,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          SignInButton(
+            text: 'Go anonymous',
+            textColor: Colors.black,
+            color: Colors.lime[300],
+            onPressed: () {},
+          ),
         ],
       ),
     );

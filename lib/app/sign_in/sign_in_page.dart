f5f16e81de165007ff753c3dbe383696e05d4243
Eventyret <simen.dehlin@gmail.com>
2020-05-18 16:41:28 +0100

    Replaced first button

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:time_tracker/app/sign_in/sign_in_button.dart';
 import 'package:time_tracker/common/custom_raised_button.dart';
 
+import 'social_sign_in_button.dart';
+
 class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -48,7 +50,8 @@ class SignInPage extends StatelessWidget {
             onPressed: () {},
           ),
           SizedBox(height: 8.0),
-          SignInButton(
+          SocialSignInButton(
+            assetName: 'assets/images/google-logo.png',
             text: 'Sign in with Google',
             textColor: Colors.black87,
             color: Colors.white,

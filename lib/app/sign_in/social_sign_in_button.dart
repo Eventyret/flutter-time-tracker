2f73f0d506aef6c402548e3177f2f487caf8e9b3
Eventyret <simen.dehlin@gmail.com>
2020-05-18 16:41:23 +0100

    Extracted and created new social sign in button

---
+++ lib/app/sign_in/social_sign_in_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker/common/custom_raised_button.dart';
+
+class SocialSignInButton extends CustomRaisedButton {
+  SocialSignInButton({
+    String assetName,
+    String text,
+    Color color,
+    Color textColor,
+    VoidCallback onPressed,
+  }) : super(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Image.asset(assetName),
+                Text(
+                  text,
+                  style: TextStyle(
+                    color: textColor,
+                    fontSize: 15.0,
+                  ),
+                ),
+                Opacity(
+                  opacity: 0,
+                  child: Image.asset(assetName),
+                ),
+              ],
+            ),
+            color: color,
+            onPressed: onPressed);
+}

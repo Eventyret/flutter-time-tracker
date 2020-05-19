ef9fd34c7a81dce67e7450218defa38715be8200
Eventyret <simen.dehlin@gmail.com>
2020-05-19 14:21:07 +0100

    Changed from Container to HomePage

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:time_tracker/app/home_page.dart';
 import 'package:time_tracker/app/sign_in/sign_in_page.dart';
 
 class LandingPage extends StatefulWidget {
@@ -25,7 +26,7 @@ class _LandingPageState extends State<LandingPage> {
         onSignIn: _updateUser,
       );
     } else {
-      return Container();
+      return HomePage();
     }
   }
 }

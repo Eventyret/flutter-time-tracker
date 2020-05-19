c35eb36ea4cb83d6ccdc3fb7208513a5381c339e
Eventyret <simen.dehlin@gmail.com>
2020-05-19 14:29:28 +0100

    Added signout functionality

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,7 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
+  const HomePage({Key key, @required this.onSignOut }) : super(key: key);
+  final VoidCallback onSignOut;
+  Future<void> _signOut() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      onSignOut();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +20,6 @@ class HomePage extends StatelessWidget {
         title: Text('Home Page'),
         actions: <Widget>[
           FlatButton(
-            onPressed: () {},
             child: Text(
               'Logout',
               style: TextStyle(
@@ -18,6 +27,7 @@ class HomePage extends StatelessWidget {
                 fontSize: 18.0,
               ),
             ),
+              onPressed: _signOut,
           )
         ],
       ),

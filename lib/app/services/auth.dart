dd0894deecd7ff40a07f909c5e6f465558ca5aae
Eventyret <simen.dehlin@gmail.com>
2020-05-22 07:58:47 +0100

    Added an abstract AuthBase

--- lib/app/services/auth.dart
+++ lib/app/services/auth.dart
@@ -3,11 +3,16 @@ import 'package:flutter/foundation.dart';
 
 class User {
   User({@required this.uid});
-
   final String uid;
 }
 
-class Auth {
+abstract class AuthBase {
+  Future<User> currentUser();
+  Future<User> signInAnonymously();
+  Future<void> signOut();
+}
+
+class Auth implements AuthBase {
   final _firebaseAuth = FirebaseAuth.instance;
   User _userFromFirebase(FirebaseUser user) {
     if (user == null) {
@@ -16,16 +21,19 @@ class Auth {
     return User(uid: user.uid);
   }
 
+  @override
   Future<User> currentUser() async {
     final user = await _firebaseAuth.currentUser();
     return _userFromFirebase(user);
   }
 
+  @override
   Future<User> signInAnonymously() async {
     final authResults = await _firebaseAuth.signInAnonymously();
     return _userFromFirebase(authResults.user);
   }
 
+  @override
   Future<void> signOut() async {
     await _firebaseAuth.signOut();
   }

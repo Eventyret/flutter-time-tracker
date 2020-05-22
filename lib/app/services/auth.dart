ef9781ccb6f5b44361b2204c0f0fd6ba5cf26776
Eventyret <simen.dehlin@gmail.com>
2020-05-22 07:12:24 +0100

    Added 3 methods to use

--- lib/app/services/auth.dart
+++ lib/app/services/auth.dart
@@ -1,7 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class Auth {
-  Future<FirebaseUser> currentUser() async {}
-  Future<FirebaseUser> signInAnonymously() async {}
-  Future<Void> signOut() async {}
+  Future<FirebaseUser> currentUser() async {
+    return await FirebaseAuth.instance.currentUser();
+  }
+
+  Future<FirebaseUser> signInAnonymously() async {
+    final authResults = await FirebaseAuth.instance.signInAnonymously();
+    return authResults.user;
+  }
+
+  Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
 }

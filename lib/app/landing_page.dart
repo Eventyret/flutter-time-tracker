cd9a42e9519c81f599bfa2d8ae86f45f5a03378b
Eventyret <simen.dehlin@gmail.com>
2020-05-22 08:03:31 +0100

    Decoupled firebase from Landing page

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker/app/home_page.dart';
+import 'package:time_tracker/app/services/auth.dart';
 import 'package:time_tracker/app/sign_in/sign_in_page.dart';
 
 class LandingPage extends StatefulWidget {
-  LandingPage({Key key}) : super(key: key);
+  final AuthBase auth;
+  LandingPage({Key key, @required this.auth}) : super(key: key);
 
   @override
   _LandingPageState createState() => _LandingPageState();
@@ -20,7 +22,7 @@ class _LandingPageState extends State<LandingPage> {
   }
 
   Future<void> _checkCurrentUser() async {
-    FirebaseUser user = await FirebaseAuth.instance.currentUser();
+    User user = await widget.auth.currentUser();
     _updateUser(user);
   }
 

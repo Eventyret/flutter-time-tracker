ba7dc659e96f6015766e520b6715a4b744e09168
Eventyret <simen.dehlin@gmail.com>
2020-05-23 11:42:10 +0100

    Added some error messages

--- lib/app/services/auth.dart
+++ lib/app/services/auth.dart
@@ -48,14 +48,25 @@ class Auth implements AuthBase {
     if (googleAccount != null) {
       GoogleSignInAuthentication googleAuth =
           await googleAccount.authentication;
-      final authResult = await _firebaseAuth.signInWithCredential(
-        GoogleAuthProvider.getCredential(
-            idToken: googleAuth.idToken, accessToken: googleAuth.accessToken),
-      );
-      return _userFromFirebase(authResult.user);
+      if (googleAuth.accessToken != null && googleAuth.idToken != null) {
+        final authResult = await _firebaseAuth.signInWithCredential(
+          GoogleAuthProvider.getCredential(
+            idToken: googleAuth.idToken,
+            accessToken: googleAuth.accessToken,
+          ),
+        );
+        return _userFromFirebase(authResult.user);
+      } else {
+        throw PlatformException(
+          code: 'ERROR_MISSING_GOOGLE_AUTH_TOKEN',
+          message: 'Missing Google Auth Token',
+        );
+      }
     } else {
       throw PlatformException(
-          code: 'ERROR_ABORTED_BY_USER', message: 'Sign in aborted by user');
+        code: 'ERROR_ABORTED_BY_USER',
+        message: 'Sign in aborted by user',
+      );
     }
   }
 

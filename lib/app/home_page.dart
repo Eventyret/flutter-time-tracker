9efc44efc7fef80366a16f70375d66cdc27fa681
Eventyret <simen.dehlin@gmail.com>
2020-05-23 08:38:39 +0100

    Removed onSignOut method no longer needed

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:time_tracker/app/services/auth.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key, @required this.onSignOut, @required this.auth }) : super(key: key);
-  final VoidCallback onSignOut;
+  const HomePage({Key key, @required this.auth}) : super(key: key);
   final AuthBase auth;
   Future<void> _signOut() async {
     try {
       await auth.signOut();
-      onSignOut();
     } catch (e) {
       print(e.toString());
     }
@@ -28,7 +26,7 @@ class HomePage extends StatelessWidget {
                 fontSize: 18.0,
               ),
             ),
-              onPressed: _signOut,
+            onPressed: _signOut,
           )
         ],
       ),

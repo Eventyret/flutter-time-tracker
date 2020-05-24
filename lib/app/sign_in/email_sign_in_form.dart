986bb00b0e2b5d0884d0c2447fcbfe385eb7766b
Eventyret <simen.dehlin@gmail.com>
2020-05-24 21:34:17 +0100

    Created constructor with auth injected

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker/app/services/auth.dart';
 import 'package:time_tracker/common/form_submit_button.dart';
 
 enum EmailSignInFormType { signIn, register }
 
 class EmailSignInForm extends StatefulWidget {
+  final AuthBase auth;
+
+  const EmailSignInForm({Key key, @required this.auth}) : super(key: key);
   @override
   _EmailSignInFormState createState() => _EmailSignInFormState();
 }

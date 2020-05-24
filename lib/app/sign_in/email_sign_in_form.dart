a4a9d0d6ceada6a4b83850e87891bd13e73e4c4e
Eventyret <simen.dehlin@gmail.com>
2020-05-24 20:25:03 +0100

    Added some spacing

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -11,14 +11,17 @@ class EmailSignInForm extends StatelessWidget {
           hintText: 'test@test.com',
         ),
       ),
+      SizedBox(height: 8.0,),
       TextField(
         decoration: InputDecoration(labelText: 'Password'),
         obscureText: true,
       ),
+      SizedBox(height: 8.0,),
       RaisedButton(
         child: Text('Sign In'),
         onPressed: () {},
       ),
+      SizedBox(height: 8.0,),
       FlatButton(
         onPressed: () {},
         child: Text('Need an account? Register'),
@@ -31,6 +34,7 @@ class EmailSignInForm extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisSize: MainAxisSize.min,
         children: _buildChildren(),
       ),

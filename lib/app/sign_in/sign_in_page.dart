dfbee37483d1ac8afe7cf3420a44ec6ee61dc0b0
Eventyret <simen.dehlin@gmail.com>
2020-05-24 09:18:37 +0100

    Passing build context

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -24,16 +24,14 @@ class SignInPage extends StatelessWidget {
         ),
         elevation: 2.0,
       ),
-      body: _buildContent(),
+      body: _buildContent(context),
       backgroundColor: Colors.grey[200],
     );
   }
-  
-  void _signInWithEmail(BuildContext context) {
-    
-  }
 
-  Widget _buildContent() {
+  void _signInWithEmail(BuildContext context) {}
+
+  Widget _buildContent(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(16.0),
       child: Column(
@@ -73,7 +71,7 @@ class SignInPage extends StatelessWidget {
             text: 'Sign in with Email',
             textColor: Colors.white,
             color: Colors.teal[700],
-            onPressed: () {},
+            onPressed: () => _signInWithEmail(context),
           ),
           SizedBox(
             height: 8.0,

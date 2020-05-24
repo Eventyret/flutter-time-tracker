04da220b0358f2e42ec562089d1d7965a1cd3aba
Eventyret <simen.dehlin@gmail.com>
2020-05-24 21:35:24 +0100

    Passing auth params

--- lib/app/sign_in/email_sign_in_page.dart
+++ lib/app/sign_in/email_sign_in_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker/app/services/auth.dart';
 import 'package:time_tracker/app/sign_in/email_sign_in_form.dart';
 
 class EmailSignInPage extends StatelessWidget {
-  const EmailSignInPage({Key key}) : super(key: key);
+  final AuthBase auth;
+  const EmailSignInPage({Key key, @required this.auth}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +17,7 @@ class EmailSignInPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Card(child: EmailSignInForm()),
+        child: Card(child: EmailSignInForm(auth: auth)),
       ),
       backgroundColor: Colors.grey[200],
     );

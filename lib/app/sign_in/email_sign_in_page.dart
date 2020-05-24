b727ff9a18a16d5aa5bb8b83e056033b21422429
Eventyret <simen.dehlin@gmail.com>
2020-05-24 09:02:53 +0100

    Created basic stateless widget

---
+++ lib/app/sign_in/email_sign_in_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EmailSignInPage extends StatelessWidget {
+  const EmailSignInPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
